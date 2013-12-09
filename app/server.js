b177952dadc06f73b0a59b365a8fa629b7bfc88d
Semmy Purewal <semmypurewal@gmail.com>
2013-12-08 18:22:56 -0800

    Update single-quotes to double-quotes

--- app/server.js
+++ app/server.js
@@ -1,8 +1,11 @@
-var http = require('http');
+var http = require("http"),
+    server;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!\n');
-}).listen(3000);
+server = http.createServer(function (req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Hello World!\n");
+});
 
-console.log('Server running on port 3000');
+server.listen(3000);
+
+console.log("Server running on port 3000");
